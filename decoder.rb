d73d6c7255f896aa437b2649016e912d254cf7cb
christianonoh <onohchibyk@gmail.com>
2023-06-14 22:36:25 +0100

    Add decode_word method

--- decoder.rb
+++ decoder.rb
@@ -11,4 +11,14 @@ class MorseCodeDecoder
   def self.decode_char(morse_char)
     @@morse_code_dict[morse_char] || ''
   end
+
+  def self.decode_word(morse_word)
+    morse_chars = morse_word.split(' ')
+    decoded_word = ''
+    morse_chars.each do |morse_char|
+      decoded_char = decode_char(morse_char)
+      decoded_word += decoded_char
+    end
+    decoded_word
+  end
 end
