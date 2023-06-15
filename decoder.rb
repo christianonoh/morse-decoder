659ca19a9a18f1664d19e8f9e1efec9ca16ea69c
christianonoh <onohchibyk@gmail.com>
2023-06-15 01:48:01 +0100

    Fix linter errors

--- decoder.rb
+++ decoder.rb
@@ -1,5 +1,5 @@
 class MorseCodeDecoder
-  @@morse_code_dict = {
+  @morse_code_dict = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
     '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
     '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
@@ -8,12 +8,16 @@ class MorseCodeDecoder
     '--..' => 'Z'
   }
 
-  def self.decode_char(morse_char)
-    @@morse_code_dict[morse_char] || ''
+  class << self
+    attr_reader :morse_code_dict
+
+    def decode_char(morse_char)
+      morse_code_dict[morse_char] || ''
+    end
   end
 
   def self.decode_word(morse_word)
-    morse_chars = morse_word.split(' ')
+    morse_chars = morse_word.split
     decoded_word = ''
     morse_chars.each do |morse_char|
       decoded_char = decode_char(morse_char)
@@ -21,12 +25,13 @@ class MorseCodeDecoder
     end
     decoded_word
   end
+
   def self.decode(message)
     morse_words = message.split('   ')
     decoded_message = ''
     morse_words.each do |morse_word|
       decoded_word = decode_word(morse_word)
-      decoded_message += decoded_word + ' '
+      decoded_message << "#{decoded_word} "
     end
     decoded_message.strip
   end
